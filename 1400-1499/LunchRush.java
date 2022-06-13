07a8283290feaaaa8f2003b610d2e46b99263e6f
Prajwal <prajwaldhule36@gmail.com>
2022-06-14 01:22:17 +0530

    Added program file for problem - Lunch Rush

--- 1400-1499/LunchRush.java
+++ 1400-1499/LunchRush.java
@@ -1,2 +1,34 @@
-public class LunchRush {
+import java.util.*;
+
+public class LunchRush{
+    public static void main(String[] args){
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int f = sc.nextInt();
+        int t = sc.nextInt();
+        int max;
+        if(t > k){
+            max = f - t + k;
+        }
+        else {
+            max = f;
+        }
+        for(int i=1; i<n; i++){
+            f = sc.nextInt();
+            t = sc.nextInt();
+            if(t > k){
+                if(f - t + k > max){
+                    max = f - t + k;
+                }
+            }
+            else {
+                if(f > max){
+                    max = f;
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
 }
